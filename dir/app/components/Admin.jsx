83b5556f1f03ca55c399204d94738b6a748bd68b
myers259 <myers259@purdue.edu>
2018-09-22 15:48:46 -0400

    adding inital work for admin page

---
+++ dir/app/components/Admin.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class Admin extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+
+    render() {
+        return(
+          <div></div>  
+        );
+    }
+
+}
