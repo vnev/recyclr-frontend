044061dc74525f15504d98ff22eec15ae3b3cd6e
myers259 <myers259@purdue.edu>
2018-10-04 02:12:44 -0400

    removing date and time from payment page

--- dir/app/components/CheckOut.jsx
+++ dir/app/components/CheckOut.jsx
@@ -22,7 +22,6 @@ import Axios from 'axios';
             console.log(token.id);
             console.log(token.token);
             bodyFormData.set('token', token.token.id);
-            //let token = this.props.stripe.createToken({name : 'bob'});
             Axios.post(`http://recyclr.xyz/charge`,  bodyFormData ,{headers : {'Content-Type' : 'multipart/form-data'}}).then(function(result) {
                 console.log("Purchase Complete");
             }).then(function (response) {
@@ -34,37 +33,11 @@ import Axios from 'axios';
                 console.log(response);
             });
         });
-        /*
-        console.log(token);
-        console.log(token.id);
-        console.log(token.token);
-        bodyFormData.set('token', token.id);
-        //let token = this.props.stripe.createToken({name : 'bob'});
-        Axios.post(`http://recyclr.xyz/charge`,  token,{headers : {'Content-Type' : 'multipart/form-data'}}).then(function(result) {
-            console.log("Purchase Complete");
-        }).then(function (response) {
-            //handle success
-            console.log(response);
-        })
-        .catch(function (response) {
-            //handle error
-            console.log(response);
-        });*/
-    }
-
-    addressHandle(ev) {
-        this.setState({
-            address: ev.target.value,
-        });
     }
 
     render() {
         return(
             <form onSubmit={this.submitHandle}>
-                <h2>Enter in your desired pick up location</h2>
-                <input type="text" className="form-control addressInput" 
-                     placeholder="6969 Sharmp way, Shrimp, IN" value={this.state.address} onChange={this.addressHandle}>
-                </input>
                 <CardSection />
                 <div className="row align-content-left">
                     <div className="col-3">
