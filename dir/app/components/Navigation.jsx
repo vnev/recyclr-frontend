e237343e90ba451b930b38349a82d2fe0ebe1aa6
claker15 <claker@purdue.edu>
2018-10-28 18:56:38 -0400

    fixed navbar items

--- dir/app/components/Navigation.jsx
+++ dir/app/components/Navigation.jsx
@@ -9,21 +9,53 @@ export default class Navigation extends React.Component {
         super(props);
         this.state= {
             isLogedIn: false,
+            isCompany: 'm',
         };
         
         this.logOut = this.logOut.bind(this);
+    }
+    getAccType() {
+        let _this = this;
+        axios.get(`http://recyclr.xyz/user/${window.localStorage.getItem('userid')}`, {headers:{'Authorization': 'Bearer ' + window.localStorage.getItem('token')}})
+            .then(function(result) {
+                if (result.data.is_company) {
+                    _this.setState({
+                        isCompany: 'y',
+                    });
+                }
+                else {
+                    _this.setState({
+                        isCompany: 'n',
+                    });
+                }
+            });
+    }
+    componentDidUpdate(prevProps, prevState, shit) {
+        if (window.localStorage.getItem('userid') === null && this.state.isCompany.length === 0) {
+            this.setState({
+                isCompany: 'm'
+            });
+        }
+        else if (window.localStorage.getItem('userid') !== null && this.state.isCompany === 'm') {
+            this.getAccType();
+        }
+        else if (prevState.isCompany !== this.state.isCompany) {
+            this.forceUpdate();
+        }
     }
     logOut() {
         let requestObject = {
             user_id: parseInt(window.localStorage.getItem('userid')),
         }
-        axios.post(`http://recyclr.xyz/user/logout`, requestObject, {headers:{'Authorization': 'Bearer ' + window.localStorage.getItem('token'),'Access-Control-Allow-Origin':'*'}}).then(function(result) {
-            console.log(result);
-            window.localStorage.removeItem('token');
-            window.localStorage.removeItem('userid');
-            window.localStorage.removeItem('username');
-            window.localStorage.removeItem('useremail');
-            history.push('/auth');
+        let _this = this;
+        axios.post(`http://recyclr.xyz/user/logout`, requestObject, {headers:{'Authorization': 'Bearer ' + window.localStorage.getItem('token'),'Access-Control-Allow-Origin':'*'}})
+        .then(function(result) {
+            window.localStorage.clear();
+            _this.setState({
+                isCompany: 'm',
+            }, () => {
+                    history.push('/auth')
+                });
         }).catch(function(error) {
             console.log(error);
         })
@@ -31,16 +63,55 @@ export default class Navigation extends React.Component {
 
     render() {
         let but;
-            if (window.localStorage.getItem('username') === null) {
-                but =   <li className="nav-item">
-                            <a className="nav-link" href="/auth">Log In</a>
+        if (window.localStorage.getItem('userid') === null) {
+            but =   <li className="nav-item">
+                        <a className="nav-link" href="/auth">Log In</a>
+                    </li>
+        }
+        else {
+            but =  <li className="nav-item">
+                        <a className="nav-link" onClick={this.logOut}>Sign Out</a>
+                    </li>
+        }
+        var view;
+        if (this.state.isCompany === 'y') {
+            view = <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <a className="nav-link" href="/listings">Listings</a>
                         </li>
-            }
-            else {
-                but =  <li className="nav-item">
-                            <a className="nav-link" onClick={this.logOut} href="/auth">Sign Out</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/chatselect">Chat</a>
                         </li>
-            }
+                        <li className="nav-item">
+                            <a className="nav-link" href="/settings">My Settings</a>
+                        </li>
+                    </ul>
+                    
+            </div>;
+        }
+        else if (this.state.isCompany === 'n') {
+            view = <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <a className="nav-link" href="/createListing">Create Listing</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/chatselect">Chat</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/progress">My Progress</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/settings">Settings</a>
+                        </li>
+                    </ul>
+                    
+            </div>;
+        }
+        else if (this.state.isCompany === 'm' || window.localStorage.getItem('userid') === null){
+            view = null;
+        }
         return(
             
             <div className="navbar navbar-expand-lg navbar-light bg-light">
@@ -51,24 +122,12 @@ export default class Navigation extends React.Component {
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item">
-                            <a className="nav-link" href="/admin">Admin</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/listings">Listings</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/progress">My Progress</a>
-                        </li>
-                    </ul>
-                    <ul className="navbar-nav my-2 my-lg-0">
+                {view}
+                <ul className="navbar-nav my-2 my-lg-0">
                         {but}
-                    </ul>
+                </ul>
                 </div>
-            </div>
+            
 
         );
     }
