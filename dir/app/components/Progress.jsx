6b1aed4460aeb86ce2778970c5e1f7ddbd395f1e
Vedant Nevetia <nevetia.vedant@gmail.com>
2018-10-28 17:40:17 -0400

    New UI for main pages

--- dir/app/components/Progress.jsx
+++ dir/app/components/Progress.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FacebookShareButton, FacebookIcon}from 'react-share'
+import { FacebookShareButton, FacebookIcon } from 'react-share'
 import history from './history.js'
 import axios from 'axios'
 //TODO: REPLACE DUMMY VALUES WITH DATABASE VALUES
@@ -12,22 +12,22 @@ export default class Progress extends Component {
     }
     this.hasShared = this.hasShared.bind(this);
   }
-   componentDidMount() {
-     if (window.localStorage.getItem('username') === null) {
-       history.push('/auth');
-     }
+  componentDidMount() {
+    if (window.localStorage.getItem('username') === null) {
+      history.push('/auth');
+    }
     console.log((window.localStorage.getItem('userid')));
     //  console.log(user_id);
-      let _this = this;
-      //make get request using stored email/username
-      axios.get('http://recyclr.xyz/user/progress/' + window.localStorage.getItem('userid'),{headers:{'Authorization': 'Bearer ' + window.localStorage.getItem('token'),'Access-Control-Allow-Origin':'*'}})
-      .then(function(result) {
-          console.log(result);
-          _this.setState({list: result.data});
-      }).catch(function(error) {
+    let _this = this;
+    //make get request using stored email/username
+    axios.get('http://recyclr.xyz/user/progress/' + window.localStorage.getItem('userid'), { headers: { 'Authorization': 'Bearer ' + window.localStorage.getItem('token'), 'Access-Control-Allow-Origin': '*' } })
+      .then(function (result) {
+        console.log(result);
+        _this.setState({ list: result.data });
+      }).catch(function (error) {
         console.log(error);
       })
-      //set list = returned json objects list = results.data
+    //set list = returned json objects list = results.data
   }
   hasShared() {
     let _this = this
@@ -36,24 +36,30 @@ export default class Progress extends Component {
     });
     console.log(this.state.shared);
   }
-    render() {
-      return(
-        <div>
-          <h1>My Recyclr Progress</h1>
-          <h3>Total number of Recyclr listings: {this.state.list.length}</h3>
-          <div id="socialWrapper" onClick={this.hasShared}>
-          <FacebookShareButton
-            url='http://recyclr.xyz'
-            quote="My Recyclr Progress: Total Number of recycling:  {_this.state.list.length}"
-            id="socialButton"
-          >
-          <FacebookIcon
-            size={32}
-            square
-          />
-          </FacebookShareButton>
+  render() {
+    return (
+      <div className="row">
+        <div className="col-md-12 text-center">
+          <div className="card">
+            <div className="card-body">
+              <h3 className="card-title">Your Recycling Progress</h3>
+              <h5 style={{ fontWeight: "normal" }}>Total listings: <b>{this.state.list.length}</b></h5>
+              <div id="socialWrapper" onClick={this.hasShared}>
+                <FacebookShareButton
+                  url='http://recyclr.xyz'
+                  quote="My Recyclr Progress: Total Number of recycling:  {_this.state.list.length}"
+                  id="socialButton"
+                >
+                  <FacebookIcon
+                    size={32}
+                    square
+                  />
+                </FacebookShareButton>
+              </div>
+            </div>
           </div>
         </div>
-      )
-    }
+      </div>
+    )
+  }
 }
