337faa99660e1bcb19a5cc3ab95e2f757485f9ae
cory <claker@pop-os.localdomain>
2018-12-07 15:41:46 -0500

    d

--- dir/app/components/api.js
+++ dir/app/components/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 export default axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'http://recyclr.xyz/api',
     headers: {
         'Authorization': 'Bearer ' + window.localStorage.getItem('token'),
     }
